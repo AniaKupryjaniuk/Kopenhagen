a7f04cc033ffd580e4d9b007a664b3e1ad31de40
Zamira Grigiene <Zamira@Zamiras-MacBook-Pro.local>
2020-04-26 15:45:31 +0300

    added filtering for events when selecting different event types in the TYPE selector. When selected option TYPE again all events are displayed

--- scripts/events.js
+++ scripts/events.js
@@ -6,9 +6,17 @@
 // 5. Show one event in a different page      |
 // -------------------------------------------
 
-window.addEventListener('DOMContentLoaded', getData);
+window.addEventListener('DOMContentLoaded', init);
 
 const eventsLink = "http://multidani.eu/shop/wp-json/wp/v2/event"
+const eventTypesLink = "http://multidani.eu/shop/wp-json/wp/v2/eventtype"
+
+function init() {
+    getData()
+    getEventTypes()
+}
+
+//------------------Display all events----------------------
 
 function getData() {
     fetch(eventsLink)
@@ -28,4 +36,38 @@ function showEvent(event) {
     copy.querySelector(".img-link").href = "singleEvent.html?event_id=" + event.id;
 
     document.querySelector(".event-grid-wrapper").appendChild(copy);
+}
+
+//---------Display all event types in selection box----------
+
+function getEventTypes() {
+    fetch(eventTypesLink)
+        .then(res => res.json())
+        .then(setEventTypes)
+}
+
+function setEventTypes(eventTypes) {
+    eventTypes.forEach(setTypeSelection)
+}
+
+function setTypeSelection(type) {
+    console.log(type)
+    const typeOption = document.createElement("option");
+    typeOption.innerHTML = type.name;
+    typeOption.value = type.id;
+    document.querySelector(".type-selection").appendChild(typeOption);
+}
+
+function eventFilter() {
+    //clear the current displayed events:
+    document.querySelector(".event-grid-wrapper").innerHTML = "";
+
+    var IDOfType = document.querySelector(".type-selection").value
+    if (IDOfType == "none") {
+        getData()
+    } else {
+        fetch("http://multidani.eu/shop/wp-json/wp/v2/event?eventtype=" + IDOfType)
+            .then(res => res.json())
+            .then(handleData)
+    }
 }
