ee84e2ad42d8624891a1b6a767b0d717dc36d5ee
Zamira Grigiene <Zamira@Zamiras-MacBook-Pro.local>
2020-05-01 14:31:23 +0300

    single event page

---
+++ scripts/singleEvent.js
@@ -0,0 +1,20 @@
+const urlParams = new URLSearchParams(window.location.search);
+const eventID = urlParams.get("event_id");
+window.addEventListener('DOMContentLoaded', getData);
+
+function getData() {
+    fetch("http://multidani.eu/shop/wp-json/wp/v2/event/" + eventID + "?_embed")
+        .then(res => res.json())
+        .then(showEvent)
+}
+
+function showEvent(event) {
+    console.log(event)
+    document.querySelector(".event_image").src = event.image.guid;
+    document.querySelector(".title").innerHTML = event.title.rendered;
+    document.querySelector(".location").innerHTML = "Location: " + event.location;
+    document.querySelector(".type").innerHTML = "Type: " + event._type;
+    document.querySelector(".author").innerHTML = "Author: " + event._name;
+    document.querySelector(".description").innerHTML = event.info;
+    document.querySelector(".date").innerHTML = "Date: " + event.time;
+}
